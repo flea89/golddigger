5fdfc9e7fdb4d3a7b399d21ea7ea2107a80bbb9d
Paolo Chillari <killer.paolo@gmail.com>
2025-05-23 15:00:25 +0200

    A random change

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Post count {postCount}</h1>
-        <h1> Create Post</h1>
+        <h1> Create an even more AWESOME Post</h1>
         <CreatePost createPostAction={createPost} />
       </main>
     </div>
